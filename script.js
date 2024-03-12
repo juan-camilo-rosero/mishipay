3ba9eaea0fcea3c01b5e337bd76c4d30023cddd2
juan-camilo-rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-03-12 12:48:45 -0500

    Add files via upload
    Crear div sign up

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const d = document
+import { changeScreen } from "./transitions.js";
+
+const d = document,
+$signUpHeader = d.querySelector(".header-sign-up"),
+$signUpHS = d.querySelector(".hero-section-sign-up")
 
 d.addEventListener("DOMContentLoaded", e => {
     console.log("Hola uwu");
+    $signUpHeader.addEventListener("click", e => changeScreen("main", "#sign-up"))
+    $signUpHS.addEventListener("click", e => changeScreen("main", "#sign-up"))
 })
