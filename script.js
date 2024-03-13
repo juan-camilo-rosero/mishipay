cb0ed337435a93375cec73e6f045a56bb8277abc
juan-camilo-rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-03-12 20:49:57 -0500

    Add files via upload
    Añadir funcionalidad a la landing page para que al presionar los botones de iniciar sesión, comenzar ahora o crear cuenta aparezcan los formularios

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ import { changeScreen } from "./transitions.js";
 
 const d = document,
 $signUpHeader = d.querySelector(".header-sign-up"),
-$signUpHS = d.querySelector(".hero-section-sign-up")
+$signUpHS = d.querySelector(".hero-section-sign-up"),
+$closeSignUp = d.querySelector("#sign-up .close"),
+$loginHeader = d.querySelector(".header-login"),
+$LoginHS = d.querySelector(".hero-section-login"),
+$closeLogin = d.querySelector("#login .close")
 
 d.addEventListener("DOMContentLoaded", e => {
-    console.log("Hola uwu");
     $signUpHeader.addEventListener("click", e => changeScreen("main", "#sign-up"))
+    $signUpHeader.addEventListener("click", e => changeScreen("#login", "#sign-up"))
     $signUpHS.addEventListener("click", e => changeScreen("main", "#sign-up"))
+    $closeSignUp.addEventListener("click", e => changeScreen("#sign-up", "main"))
+    $loginHeader.addEventListener("click", e => changeScreen("main", "#login"))
+    $loginHeader.addEventListener("click", e => changeScreen("#sign-up", "#login"))
+    $LoginHS.addEventListener("click", e => changeScreen("main", "#login"))
+    $closeLogin.addEventListener("click", e => changeScreen("#login", "main"))
 })
