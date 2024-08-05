88a5c81edcb16cbda5a079beaf882325f3ebad85
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-08-05 10:51:37 -0500

    Add files via upload

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,6 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { SectionContext } from "../context/SectionContext"
+import { UserContext } from "../context/UserContext"
+import { DBContext } from "../context/DBContext"
+import { signOut } from "firebase/auth";
+
 function Account() {
+  const {account, setAccount} = useContext(SectionContext)
+  const {setName, name, email} = useContext(UserContext)
+  const {changeUsername} = useContext(DBContext)
+  const [username, setUsername] = useState(name)
+  const [loading, setLoading] = useState(false)
+  useEffect(() => {
+    setUsername(name)
+  }, [])
+
+  const handleSubmit = async e => {
+    e.preventDefault()
+    setLoading(true)
+    await changeUsername(email, username)
+    setLoading(false)
+  }
   return (
-    <div>Account</div>
+    <section className={`w-screen h-screen bg-black z-50 bg-opacity-30 flex items-center justify-center transition-all ${(account) ? "fixed" : "hidden"}`}>
+      <div className="bg-secondary w-4/5 pb-10 pt-2 rounded-xl p-2 flex flex-col items-end md:w-1/2 lg:w-[25rem]">
+        <RiCloseCircleFill className="text-4xl text-primary cursor-pointer" onClick={e => setAccount(false)}/>
+        <form className="w-full px-8" onSubmit={handleSubmit}>
+          <label className="font-semibold text-lg lg:text-xl">Nombre:</label>
+          <input type="text" className="w-full px-2 py-1 rounded-lg border-2 border-black text-lg font-semibold mt-3 lg:px-3 lg:text-xl lg:py-2 bg-secondary outline-none" value={username} onChange={e => setUsername(e.target.value)}/>
+          <button type="submit" className={`w-full bg-primary p-2 rounded-lg text-secondary font-semibold text-lg mt-6 lg:mt-8 ${(loading) ? "opacity-50" : "opacity-100"}`}>{(loading) ? "Cargando..." : "Cambiar nombre"}</button>
+        </form>
+          <button className="w-full bg-third p-2 rounded-lg text-primary font-semibold text-lg mt-3 lg:mt-4" onClick={async e => {
+            await signOut()
+            window.location.reload()
+          }}>Cerrar sesión</button>
+      </div>
+    </section>
   )
 }
 
--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,15 +1,17 @@
+import { RiAccountCircleFill } from "react-icons/ri";
 import { useContext } from "react"
 import { UserContext } from "../context/UserContext"
 import { SectionContext } from "../context/SectionContext"
 
 function Balance() {
     const {money} = useContext(UserContext)
-    const {setSection} = useContext(SectionContext)
+    const {setSection, setAccount} = useContext(SectionContext)
   return (
     <div className='w-full h-[30vh] lg:h-[25vh] bg-primary flex flex-col items-center justify-center gap-3 lg:items-start lg:px-10'>
       <p className="text-secondary font-semibold text-sm">Balance actual</p>
       <p className="text-5xl text-secondary font-semibold">{money}</p>
       <button onClick={() => setSection("send")} className="lg:hidden py-2 px-16 bg-secondary text-primary font-semibold text-xl rounded-md mt-4">Realizar pago</button>
+      <RiAccountCircleFill className="fixed top-5 right-5 text-4xl text-secondary lg:opacity-85 transition-all lg:hover:opacity-100 md:right-10 md:top-6 md:text-5xl cursor-pointer" onClick={e => setAccount(true)}/>
     </div>
   )
 }
