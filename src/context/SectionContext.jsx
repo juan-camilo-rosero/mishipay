6dcde13761f0ab4b3670186bc8c27b0ace7670c1
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-09-26 12:28:33 -0500

    Add files via upload

--- src/context/SectionContext.jsx
+++ src/context/SectionContext.jsx
@@ -5,13 +5,16 @@ export const SectionContext = createContext()
 export function SectionContextProvider(props) {
     const [section, setSection] = useState("history")
     const [account, setAccount] = useState(false)
+    const [map, setMap] = useState(false)
 
     return (
         <SectionContext.Provider value={{
             section,
             setSection,
             account,
-            setAccount
+            setAccount,
+            map,
+            setMap
         }}>
             {props.children}
         </SectionContext.Provider>
