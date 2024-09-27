19ca9ba4c9f5078a2533d124129412a0d4a0619c
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2024-09-27 17:58:56 -0500

    Arreglar router

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Fallback a index.html para todas las rutas no encontradas (SPA)
+    historyApiFallback: true,
+  },
 })
